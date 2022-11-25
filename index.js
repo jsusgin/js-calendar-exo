5502bcfa7c3645f8fce220d56f48645bbe7ad98b
jsusgin <118804647+jsusgin@users.noreply.github.com>
2022-11-25 14:34:46 +0100

    2 files modified

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import {
   addDays,
   addWeeks,
   addMonths,
+  eachDayOfInterval,
+  endOfMonth,
+  endOfWeek,
   format,
   getDate,
   getDay,
@@ -9,14 +12,15 @@ import {
   getWeek,
   getMonth,
   getYear,
-  startOfToday,
+  startOfMonth,
+  startOfWeek,
 } from 'date-fns';
 
 const format_style = 'eeee dd MMMM yyyy';
 const format_ = (date) => format(date, format_style);
 
-const today = startOfToday();
-var current_date = today;
+const today = new Date();
+var current = today;
 
 function init() {
   console.log(format_(today));
@@ -29,30 +33,33 @@ function init() {
 $(document).ready(init);
 
 function display_days() {
-  var length = getDaysInMonth(current_date);
-  var first_day = new Date(getYear(current_date), getMonth(current_date), 1);
-  var last_day = new Date(getYear(current_date), getMonth(current_date), length);
+  var first_day = startOfMonth(current);
+  var last_day = endOfMonth(current);
   console.log(format_(first_day));
   console.log(format_(last_day));
-  console.log(getWeek(first_day));
-  console.log(getWeek(last_day));
-  for (let w=getWeek(first_day);w<=getWeek(last_day);w++) {
+  for (let w = getWeek(first_day); w <= getWeek(last_day); w++) {}
+  var last_sunday = startOfWeek(first_day);
+  console.log(format_(last_sunday));
+  var next_saturday = endOfWeek(last_day);
+  console.log(format_(next_saturday));
 
-  }
-  find_last_sunday(first_day);
+  const weekDays = eachDayOfInterval({
+    start: startOfWeek(today),
+    end: endOfWeek(today),
+  });
+
+  const monthDays = eachDayOfInterval({
+    start: last_sunday,
+    end: next_saturday,
+  });
+
+  console.log(weekDays.map((elt) => format_(elt)));
+  console.log(monthDays.map((elt) => format_(elt)));
 }
 
 function create_nav_buttons() {
-  $('#last_month').text(format(addMonths(current_date, -1), 'MMMM'));
-  $('#next_month').text(format(addMonths(current_date, +1), 'MMMM'));
-  $('nav h2').text(format(current_date, "MMMM"));
-  $('nav h1').text(format_(current_date));
-}
-
-function find_last_sunday(date) {
-  console.log(format_(date), getDay(date));
-  while (getDay(date) != 0) {
-    date = addDays(date, -1);
-    console.log(format_(date));
-  }
+  $('#last_month').text(format(addMonths(current, -1), 'MMMM'));
+  $('#next_month').text(format(addMonths(current, +1), 'MMMM'));
+  $('nav h2').text(format(current, 'MMMM'));
+  $('nav h1').text(format_(current));
 }
