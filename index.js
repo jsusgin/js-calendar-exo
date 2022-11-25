f62973d3fe55b3b2bfe775880bd459557a48e752
jsusgin <118804647+jsusgin@users.noreply.github.com>
2022-11-25 14:09:15 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import {
+  addDays,
+  addWeeks,
+  addMonths,
   format,
   getDate,
   getDay,
   getDaysInMonth,
+  getWeek,
   getMonth,
   getYear,
   startOfToday,
@@ -12,32 +16,43 @@ const format_style = 'eeee dd MMMM yyyy';
 const format_ = (date) => format(date, format_style);
 
 const today = startOfToday();
+var current_date = today;
 
 function init() {
   console.log(format_(today));
-  console.log(date_to_obj(today));
-  console.log(format_(obj_to_date(date_to_obj(today))));
   console.log(getDaysInMonth(today));
+  console.log(getWeek(today));
   create_nav_buttons();
+  display_days();
 }
 
 $(document).ready(init);
 
-function date_to_obj(date) {
-  return {
-    day_week: getDay(date),
-    day_month: getDate(date),
-    month: getMonth(date),
-    year: getYear(date),
-  };
-}
+function display_days() {
+  var length = getDaysInMonth(current_date);
+  var first_day = new Date(getYear(current_date), getMonth(current_date), 1);
+  var last_day = new Date(getYear(current_date), getMonth(current_date), length);
+  console.log(format_(first_day));
+  console.log(format_(last_day));
+  console.log(getWeek(first_day));
+  console.log(getWeek(last_day));
+  for (let w=getWeek(first_day);w<=getWeek(last_day);w++) {
 
-function obj_to_date(obj) {
-  return new Date(obj.year, obj.month, obj.day_month);
+  }
+  find_last_sunday(first_day);
 }
 
 function create_nav_buttons() {
-  month = getMonth(today);
-  console.log(month);
-  $('#last_month').text(month);
+  $('#last_month').text(format(addMonths(current_date, -1), 'MMMM'));
+  $('#next_month').text(format(addMonths(current_date, +1), 'MMMM'));
+  $('nav h2').text(format(current_date, "MMMM"));
+  $('nav h1').text(format_(current_date));
+}
+
+function find_last_sunday(date) {
+  console.log(format_(date), getDay(date));
+  while (getDay(date) != 0) {
+    date = addDays(date, -1);
+    console.log(format_(date));
+  }
 }
