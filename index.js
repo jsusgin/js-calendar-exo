2c456f38b997992b127a46c522c6983cc13e060f
jsusgin <118804647+jsusgin@users.noreply.github.com>
2022-11-25 14:38:27 +0100

    1 file modified

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ const today = new Date();
 var current = today;
 
 function init() {
-  console.log(format_(today));
-  console.log(getDaysInMonth(today));
-  console.log(getWeek(today));
   create_nav_buttons();
   display_days();
 }
@@ -33,28 +30,22 @@ function init() {
 $(document).ready(init);
 
 function display_days() {
+  let days_arr = get_days();
+  console.log(days_arr.map((elt) => format_(elt)));
+}
+
+function get_days() {
   var first_day = startOfMonth(current);
   var last_day = endOfMonth(current);
-  console.log(format_(first_day));
-  console.log(format_(last_day));
-  for (let w = getWeek(first_day); w <= getWeek(last_day); w++) {}
   var last_sunday = startOfWeek(first_day);
-  console.log(format_(last_sunday));
   var next_saturday = endOfWeek(last_day);
-  console.log(format_(next_saturday));
-
-  const weekDays = eachDayOfInterval({
-    start: startOfWeek(today),
-    end: endOfWeek(today),
-  });
 
   const monthDays = eachDayOfInterval({
     start: last_sunday,
     end: next_saturday,
   });
 
-  console.log(weekDays.map((elt) => format_(elt)));
-  console.log(monthDays.map((elt) => format_(elt)));
+  return monthDays;
 }
 
 function create_nav_buttons() {
